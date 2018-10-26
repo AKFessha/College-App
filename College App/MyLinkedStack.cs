949c075035e90615a867b27d6f25bf7b4b6787d1
Awet Fessha <awetkifle94@gmail.com>
2018-10-26 11:16:02 +0100

    add constructor and new list

--- College App/MyLinkedStack.cs
+++ College App/MyLinkedStack.cs
@@ -8,18 +8,21 @@ namespace College_App
 {
     class MyLinkedStack : IStackADT
     {
-        IntLinkedList list;
+        private DataStructure list;
         // default constructor that sets head of list to null
-
+        public MyLinkedStack()
+        {
+            list = new DataStructure();
+        }
         //adds value to list appropriately for a 
         public void push(int value) {
-
+            list.addFirst(value);
         }
 
         //remove and return value from list 
 
         public int pop() {
-            return 3;
+           return list.removeFirst();
         }
         //returns true if the stack is empty 
         public Boolean isEmpty()
